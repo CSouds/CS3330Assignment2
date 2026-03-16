263ef49a5478206a9cbb2bf0d0842e8a81fc2182
Connor Souders <cosouders@yahoo.com>
2026-03-16 17:09:08 -0500

    Student and board

--- src/assignment2/QuestBoard.java
+++ src/assignment2/QuestBoard.java
@@ -1,4 +1,35 @@
+import java.util.HashSet;
+import java.util.List;
 
 public class QuestBoard {
-
+	Map<Integer, Quest> questById; = new Map<>;
+	Map<Student, List<Quest>> assignments; = new Map<>;
+	
+	public QuestBoard()
+	{
+		questById = new Map<>;
+		assignments = new Map<>;
+	}
+	
+	public void addQuest(Quest q)
+	{
+		qId = q.getId();
+		if(questById.containsValue(qId))
+			throw new IllegalArgumentException("Id taken by other quest.");
+		questById.put(qId, q);
+	}
+	
+	public Quest findQuest(int id)
+	{
+		return questById.get(id); // returns null if missing
+	}
+	
+	public assignQuest(Student s, int questId)
+	{
+		q = findQuest(questId);
+		
+		if(!(assignments.containsValue(s)))
+			assignments.put(s, new ArrayList<Quest>);
+		
+	}
 }
