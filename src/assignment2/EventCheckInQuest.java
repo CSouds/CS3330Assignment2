e24fcc0f5e6b919075dea52f2384177bf54aa072
Chris <cacvhm@umsystem.edu>
2026-03-16 16:45:35 -0500

    Added EventCheckInQuest and VolunteerQuest

--- src/assignment2/EventCheckInQuest.java
+++ src/assignment2/EventCheckInQuest.java
@@ -1,4 +1,48 @@
 
-public class EventCheckInQuest {
+public class EventCheckInQuest extends AbstractQuest {
+	private final String eventName;
+	
+	public EventCheckInQuest(int id, String title. int basePoints, String eventName)
+	{
+		super(id,title,basePoints);
+		
+		if(eventName == null || eventName.isBlank())
+		{
+			throw new IllegalArgumentException("Event name must not be null or blank.");
+		}
+		
+		this.eventName = eventName;
+	}
 
+	public String getEventName()
+	{
+		return eventName;
+	}
+	
+	@Override public int completeFor(Student s)
+	{
+		if(s == null)
+		{
+			throw new IllegalArgumentException("Student must not be null.");
+		}
+		if(isCompleted())
+		{
+			throw new IllegalStateException("Quest is already completed.");
+		}
+		
+		int awarded = getBasePoints();
+		s.addPoints(awarded);
+		markCompleted();
+		return awarded;
+	}
+	
+	@Override String toString()
+	{
+		return "EventCheckInQuest{id=" + getId() +
+				", title = '" + getTitle() + '\'' +
+				", basePoints = " + getBasePoints() +
+				", completed = " + is Completed() +
+				", eventName = '" + eventName + '\'' +
+				"}";
+	}
 }
--- src/assignment2/VolunteerQuest.java
+++ src/assignment2/VolunteerQuest.java
@@ -1,4 +1,51 @@
 
-public class VolunteerQuest {
+public class VolunteerQuest extends AbstractQuest{
 
+	private final int hours;
+	
+	public VolunteerQuest(int id, String title, int basePoints, int hours)
+	{
+		super(id,title,basepoints);
+		
+		if(hours <= 0)
+		{
+			throw new IllegalArgumentException("Hours must be > 0");
+		}
+		
+		this.hours = hours;
+	}
+	
+	public int getHours()
+	{
+		return hours;
+	}
+	
+	@Override
+	public int completeFor(Student s)
+	{
+		if(s == null)
+		{
+			throw new IllegalArgumentException("Student must not be null.");
+		}
+		if(isCompleted())
+		{
+			throw new IllegalStateException("Quest is already completed");
+		}
+		
+		int awarded = getBasePoints() * hours;
+		s.addPoints(awarded);
+		markCompleted();
+		return awarded;
+	}
+	@Override
+	public String toString()
+	{
+		return "EventCheckInQuest{id=" + getId() +
+				", title = '" + getTitle() + '\'' +
+				", basePoints = " + getBasePoints() +
+				", completed = " + isCompleted() +
+				", hours = '" + hours +
+				"}";
+	}
+	
 }
